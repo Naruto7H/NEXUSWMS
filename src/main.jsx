a410ce57bd08972de3064db4e66b0d89275e9f5e
Suhail N <116971843+Naruto7H@users.noreply.github.com>
2026-05-07 12:34:04 +0530

    Add StoreProvider to main.jsx for state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
+import { StoreProvider } from './context/StoreContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* BrowserRouter MUST be the outermost wrapper for navigation to work */}
     <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <StoreProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </StoreProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
